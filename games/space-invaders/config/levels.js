bf28c2bc141826f7690db2a84cea3e51e5e1d364
Pandelis Manikas <pmanikas.social@gmail.com>
2024-11-25 01:41:25 +0200

    Fix raining fire due to negative number

--- games/space-invaders/config/levels.js
+++ games/space-invaders/config/levels.js
@@ -5,7 +5,7 @@ export default {
                 rows: Math.min(2 + Math.floor(i / 5), 6),
                 columns: Math.min(6 + i % 5, 11),
                 speed: Math.min(0.5 + i % 4, 4),
-                attackDelay: 2000 - Math.floor(i / 4) * 500,
+                attackDelay: Math.max(2000 - Math.floor(i / 4) * 500, 500),
             },
         },
     })).reduce((acc, level) => ({ ...acc, ...level }), {}),
